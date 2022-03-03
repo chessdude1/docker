12cb934ad71929e76509285ef000f9c1e7d43eb4
chessdude1 <mitia.platonow2011@yandex.ru>
2022-03-03 12:50:52 +0300

    fix: change showing result

--- front/src/App.js
+++ front/src/App.js
@@ -1,10 +1,16 @@
 import './App.css';
 
 function App() {
+
+  async function testFront() {
+    let res = await fetch('http://localhost:5000/api/users')
+    console.log(res)
+  }
+
   return (
     <div className="App">
       <header className="App-header">
-        <button onClick={() => { fetch('http://localhost:5000/api/users') }}>test backend</button>
+        <button onClick={testFront}>test backend</button>
       </header>
     </div>
   );
